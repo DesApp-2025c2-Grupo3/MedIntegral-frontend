d62df818519fa4584fdd89ad0e9c53183e1917c4
gabrielledezma21 <gabrielagustin.ledezma@estudiantes.unahur.edu.ar>
2025-09-13 17:22:32 -0300

    nueva semilla

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,35 @@
-import React, { useEffect, useState } from "react";
-import { Box, Grid, Stack } from "@mui/material";
-import { BrowserRouter } from "react-router-dom";
-import { TopMenu } from "./components/TopMenu";
-import { AppRouter } from "./AppRouter";
-import { getCurrentWeather } from "./services/WeatherService";
-import { WeatherIndicator } from "./components/WeatherIndicator";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-export function App() {
-  const [weatherData, setWeatherData] = useState();
-
-  useEffect(() => {
-    const fetchWeatherData = async () => {
-      const obtainedData = await getCurrentWeather('Buenos Aires');
-      setWeatherData(obtainedData);
-    }
-    fetchWeatherData();
-  }, []);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Stack direction='column'>
-        <Grid container direction='row'>
-          <Grid item xs={12} md={8}>
-            <TopMenu />
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <WeatherIndicator weatherData={weatherData} />
-          </Grid>
-        </Grid>
-        <Box sx={{mx: { xs: 1, md: 4 }, my: 4}}>
-          <AppRouter />
-        </Box>
-      </Stack>
-    </BrowserRouter>
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
+
+export default App
