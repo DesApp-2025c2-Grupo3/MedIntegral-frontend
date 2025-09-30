f0831f26e26b99da1e21f1635213d6f16d0ce159
MeliAlvarez14 <melialvarez180@gmail.com>
2025-09-30 11:50:14 -0300

    boton para expandir y contraer sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,60 +1,162 @@
-//Solo es una base, cambiar
 import { Link as RouterLink } from 'react-router-dom';
+import { useState } from 'react';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import MedicalInformationOutlinedIcon from '@mui/icons-material/MedicalInformationOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import FeedOutlinedIcon from '@mui/icons-material/FeedOutlined';
+import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
+import KeyboardDoubleArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftOutlined';
+import ExpandMoreOutlinedIcon from '@mui/icons-material/ExpandMoreOutlined';
+import ExpandLessOutlinedIcon from '@mui/icons-material/ExpandLessOutlined';
 import {
   Drawer,
   List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Toolbar,
+  ListItemButton,
+  Typography,
+  Grid
 } from '@mui/material';
-import PeopleIcon from '@mui/icons-material/People';
-import EventIcon from '@mui/icons-material/Event';
-import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
-import HomeIcon from '@mui/icons-material/Home';
 
-const drawerWidth = 220;
+
+
+
+const drawerWidth = 290;
+
 
 export default function Sidebar() {
+  const [open, setOpen] = useState(true);
+  const [openItems, setOpenItems] = useState([]);
+
+  const toggleDrawer = () => {
+    setOpen(!open);
+  };
+
+  const sidebarItems = [
+    {
+      key: 'afiliados',
+      label: 'Afiliados',
+      icon: <PersonOutlinedIcon />,
+      children: [
+        {
+          label: 'Ver listado',
+          icon: <FeedOutlinedIcon />,
+          route: '/afiliados/listado',
+        },
+        {
+          label: 'Agregar',
+          icon: <AddOutlinedIcon />,
+          route: '/afiliados/alta',
+        },
+      ],
+    },
+    {
+      key: 'prestadores',
+      label: 'Prestadores',
+      icon: <MedicalInformationOutlinedIcon />,
+      children: [
+        {
+          label: 'Ver listado',
+          icon: <FeedOutlinedIcon />,
+          route: '/prestadores/listado',
+        },
+        {
+          label: 'Agregar',
+          icon: <AddOutlinedIcon />,
+          route: '/prestadores/alta',
+        },
+      ],
+    },
+    {
+      key: 'agendaTurnos',
+      label: 'Agenda de turnos',
+      icon: <CalendarTodayOutlinedIcon />,
+      children: [
+        {
+          label: 'Ver listado',
+          icon: <FeedOutlinedIcon />,
+          route: '/agenda-turnos',
+        },
+        {
+          label: 'Agregar',
+          icon: <AddOutlinedIcon />,
+          route: '/agenda-turnos/alta',
+        },
+      ],
+    },
+  ];
+
+
   return (
-    <Drawer
-      variant="permanent"
-      sx={{
-        width: drawerWidth,
-        flexShrink: 0,
-        [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
-      }}
-    >
-      <Toolbar />
-      <List>
-        <ListItem button component={RouterLink} to="/">
-          <ListItemIcon>
-            <HomeIcon />
-          </ListItemIcon>
-          <ListItemText primary="Inicio" />
-        </ListItem>
-
-        <ListItem button component={RouterLink} to="/agenda-turnos/alta">
-          <ListItemIcon>
-            <EventIcon />
-          </ListItemIcon>
-          <ListItemText primary="Alta Agenda" />
-        </ListItem>
-
-        <ListItem button component={RouterLink} to="/prestadores/alta">
-          <ListItemIcon>
-            <LocalHospitalIcon />
-          </ListItemIcon>
-          <ListItemText primary="Alta Prestadores" />
-        </ListItem>
-
-        <ListItem button component={RouterLink} to="/afiliados/alta">
-          <ListItemIcon>
-            <PeopleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Alta Afiliados" />
-        </ListItem>
-      </List>
-    </Drawer>
+    <>
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: open ? drawerWidth : 60,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: open ? drawerWidth : 100,
+            boxSizing: 'border-box',
+            backgroundColor: '#0b111e',
+            color: '#fff',
+            transition: 'width 0.3s',
+            overflowX: 'hidden',
+          },
+        }}
+      >
+        <List sx={{ mt: 1, display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <ListItemButton
+            sx={{
+              mb: '2rem',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: open ? 'space-between' : 'center',
+              '&:hover': { backgroundColor: 'transparent' },
+            }}
+            onClick={toggleDrawer}
+          >
+            {open ? (
+              <>
+                <Grid
+                  sx={{
+                    display: 'contents',
+                    alignItems: 'center',
+                    gap: '3rem',
+                  }}
+                >
+                  <img
+                    src="/medIntegralLogo.png"
+                    alt="Logo MedIntegral"
+                    style={{ height: '4.2rem' }}
+                  ></img>
+                  <Typography
+                    sx={{
+                      color: '#00AEEF',
+                      margin: 0,
+                      fontSize: '1.4rem',
+                      fontWeight: '500',
+                      display: { xs: 'none', sm: 'block' },
+                    }}
+                  >
+                    Med
+                    <span style={{ color: '#FFFFFF' }}>Integral</span>
+                  </Typography>
+                </Grid>
+                <KeyboardDoubleArrowLeftOutlinedIcon />
+              </>
+            ) : (
+              <Grid sx={{ display: 'flex', alignItems: 'center' }}>
+                <img
+                  src="/medIntegralLogo.png"
+                  alt='"Logo MedIntegral"'
+                  style={{ height: '4.2rem' }}
+                ></img>
+              </Grid>
+            )}
+          </ListItemButton>
+        </List>
+      </Drawer>
+    </>
   );
 }
+
+
--- src/layout/layoutBase.jsx
+++ src/layout/layoutBase.jsx
@@ -1,15 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Box, Container } from '@mui/material';
-{
-  /*DESCOMENTAR: import Sidebar from '../components/Sidebar';*/
-}
+import Sidebar from '../components/Sidebar';
 import BreadcrumbsNav from '../components/common/BreadcrumbsNav';
 
 export default function LayoutBase() {
   return (
     <Container maxWidth="lg" sx={{ mt: 4 }}>
       <BreadcrumbsNav />
-      {/*DESCOMENTAR: <Sidebar />*/}
+      <Sidebar />
       <Box component="main" sx={{ flexGrow: 1, mt: 3 }}>
         <Outlet />
       </Box>
