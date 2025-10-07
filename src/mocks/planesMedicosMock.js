8bbaa728794078c6f1b5c9a0f2295eede18b8cb4
alymarquez <alyym.55@gmail.com>
2025-10-06 22:50:56 -0300

    Cambios en mock planes medicos

--- src/mocks/planesMedicosMock.js
+++ src/mocks/planesMedicosMock.js
@@ -3,8 +3,4 @@ export const planesMedicos = [
   { id: 2, plan: '310' },
   { id: 3, plan: '410' },
   { id: 4, plan: '510' },
-  { id: 5, plan: 'Bronce' },
-  { id: 6, plan: 'Plata' },
-  { id: 7, plan: 'Oro' },
-  { id: 8, plan: 'Platino' },
 ];
