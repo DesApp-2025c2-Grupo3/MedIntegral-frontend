4babc00563cd0422ecd42631bfd2ffcaa2568e80
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-14 12:17:52 -0300

    cambios

--- src/components/afiliados/ListadoAfiliadosTable.jsx
+++
@@ -1,3 +0,0 @@
-export default function ListadoAfiliadosTable() {
-  return <p>Prueba</p>;
-}
--- src/pages/afiliados/Listado.jsx
+++ src/pages/afiliados/Listado.jsx
@@ -1,16 +1,5 @@
-import Box from '@mui/material/Box';
-import PageListHeader from '../../components/common/lists/PageListHeader';
-import { usePageTitle } from '../../hooks/usePageTitle';
-import ListadoAfiliadosTable from '../../components/afiliados/ListadoAfiliadosTable';
+import Typography from '@mui/material/Typography';
 
-export default function PrestadoresListado() {
-  usePageTitle('MedIntegral | Listado de prestadores');
-
-  return (
-    <Box sx={{ mb: 2 }}>
-      <PageListHeader type="afiliado" />
-
-      <ListadoAfiliadosTable />
-    </Box>
-  );
+export default function AfiliadosListado() {
+  return <Typography>Listado de Afiliados</Typography>;
 }
