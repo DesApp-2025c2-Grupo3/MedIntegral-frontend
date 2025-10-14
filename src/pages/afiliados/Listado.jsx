33628b3bb97930c14ad029a0a221603d8b1681e4
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-14 12:22:37 -0300

    Estructura base para  listado de afiliados

--- src/pages/afiliados/Listado.jsx
+++ src/pages/afiliados/Listado.jsx
@@ -1,5 +1,16 @@
-import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import PageListHeader from '../../components/common/lists/PageListHeader';
+import { usePageTitle } from '../../hooks/usePageTitle';
+import ListadoAfiliadosTable from '../../components/afiliados/ListadoAfiliadosTable';
 
-export default function AfiliadosListado() {
-  return <Typography>Listado de Afiliados</Typography>;
+export default function PrestadoresListado() {
+  usePageTitle('MedIntegral | Listado de prestadores');
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <PageListHeader type="afiliado" />
+
+      <ListadoAfiliadosTable />
+    </Box>
+  );
 }
