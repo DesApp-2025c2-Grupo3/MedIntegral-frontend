3ea576daa78d8680d2509c9c0fcf7938c77e953c
MeliAlvarez14 <melialvarez180@gmail.com>
2025-10-31 13:30:29 -0300

    Refactor formato de listado de agendas y validaciones

--- src/utils/formats/agendaTurnosListado.js
+++ src/utils/formats/agendaTurnosListado.js
@@ -1,10 +1,10 @@
 export const formatAgendaTurnosListado = (data) => {
   try {
-    if (!data || !Array.isArray(data)) {
+    if (!data.items || !Array.isArray(data.items)) {
       throw new Error('La respuesta no tiene el formato esperado');
     }
 
-    const itemsFormateados = data.map((a) => {
+    const itemsFormateados = data.items.map((a) => {
       const dir = a.direccion
         ? `${a.direccion.calle || ''} ${a.direccion.altura || ''}${
             a.direccion.pisoDepto ? ', ' + a.direccion.pisoDepto : ''
